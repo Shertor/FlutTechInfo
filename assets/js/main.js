38d7a3347b835b90b619a8e5ac60f41477bb874a
Shertor <shertors@gmail.com>
2022-05-13 17:50:16 +0300

    AJAX подгрузка JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
 // ===================== NAVIGATION =====================
 const navItems = document.querySelectorAll('.nav__link[data-goto]')
 if (navItems.length > 0) {
-	navItems.forEach(item => {
+	navItems.forEach((item) => {
 		item.addEventListener('click', onNavItemClick)
 	})
 
 	function onNavItemClick(event) {
 		const navItem = event.target
 		if (navItem.dataset.goto && document.querySelector(navItem.dataset.goto)) {
-			const gotoBlock = document.querySelector(navItem.dataset.goto);
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+			const gotoBlock = document.querySelector(navItem.dataset.goto)
+			const gotoBlockValue =
+				gotoBlock.getBoundingClientRect().top +
+				pageYOffset -
+				document.querySelector('header').offsetHeight
 			window.scrollTo({
 				top: gotoBlockValue,
-				behavior: "smooth"
+				behavior: 'smooth',
 			})
 			event.preventDefault()
 		}
@@ -25,7 +28,10 @@ gotoAboutBtn.addEventListener('click', (event) => {
 	event.preventDefault()
 	const aboutSection = document.getElementById('about')
 	window.scrollTo({
-		top: aboutSection.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+		top:
+			aboutSection.getBoundingClientRect().top +
+			pageYOffset -
+			document.querySelector('header').offsetHeight,
 	})
 })
 
@@ -35,26 +41,25 @@ gotoFooterBtn.addEventListener('click', (event) => {
 	event.preventDefault()
 	const aboutSection = document.getElementById('footer')
 	window.scrollTo({
-		top: aboutSection.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+		top:
+			aboutSection.getBoundingClientRect().top +
+			pageYOffset -
+			document.querySelector('header').offsetHeight,
 	})
 })
-
 
 // открытие подменю
 
 const navArrows = document.querySelectorAll('.button--nav')
 
 if (navArrows) {
-	navArrows.forEach(item => {
-		item.addEventListener('click', ()=>{
+	navArrows.forEach((item) => {
+		item.addEventListener('click', () => {
 			const navItem = item.parentElement.parentElement
 			navItem.classList.toggle('show-sub-nav')
 		})
 	})
 }
-
-
-
 
 // ===================== OPEN SHOW NAVIGATION MENU =====================
 const navMenu = document.getElementById('nav-menu'),
@@ -152,13 +157,17 @@ function scrollActive() {
 			sectionId = current.getAttribute('id')
 
 		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-			const link = document.querySelector('.nav__link[data-goto=".' + sectionId + '"]')
+			const link = document.querySelector(
+				'.nav__link[data-goto=".' + sectionId + '"]'
+			)
 			if (link) {
 				link.classList.add('active-link')
 			}
 		} else {
-			const link = document.querySelector('.nav__link[data-goto=".' + sectionId + '"]')
-			if (link){
+			const link = document.querySelector(
+				'.nav__link[data-goto=".' + sectionId + '"]'
+			)
+			if (link) {
 				link.classList.remove('active-link')
 			}
 		}
@@ -177,7 +186,7 @@ window.addEventListener('scroll', () => {
 	}
 })
 scrollTop.addEventListener('click', () => {
-	window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+	window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
 })
 // ===================== THEME CHANGE =====================
 
@@ -213,29 +222,57 @@ themeBtn.addEventListener('click', () => {
 
 // ===================== ЗАПОЛНЕНИЕ ТАБЛИЦЫ =====================
 
-function getDataToInsert() {
-	
-	const literatureTrOpen = '<tr class="literature__tr">'
-	const literatureColumnOpen = '<td class="literature__td">'
-	const literatureColumnClose = '</td>'
-	const literatureTrClose = '</tr>'
-	
-	data = [
-		{author: 'Test', article: 'TestName', year: 1964}
-	]
-	
-	let dataToInsert = literatureTrOpen
-	
+const appendLitBtn = document.getElementById('append-literature')
+let fetchedItems = 0
+const itemsToFetch = 3
+
+const literatureTrOpen = '<tr class="literature__tr">'
+const literatureColumnOpen = '<td class="literature__td">'
+const literatureColumnClose = '</td>'
+const literatureTrClose = '</tr>'
+
+appendLitBtn.addEventListener('click', fetchAndAppendItems)
+
+async function fetchAndAppendItems(event) {
+	event.preventDefault()
+	const data = await getDataToInsert()
+	console.log(data)
+	if ('articles' in data) {
+		await appendData(data['articles'])
+	}
+}
+
+async function getDataToInsert() {
+	if (!appendLitBtn.classList.contains('_hold')) {
+		appendLitBtn.classList.add('_hold')
+		const file = '../assets/resources/literature.json'
+		let response = await fetch(file, { method: 'GET' })
+		if (response.ok) {
+			let result = await response.json()
+			appendLitBtn.classList.remove('_hold')
+			appendLitBtn.remove()
+			return result
+		} else {
+			alert('Ошибка загрузки списка литературы')
+		}
+	}
+	return none
+}
+
+async function appendData(data) {
+	let dataToInsert = ''
+
 	data.forEach((item) => {
+		dataToInsert += literatureTrOpen
 		const keys = Object.keys(item)
 		keys.forEach((key) => {
-			dataToInsert += literatureColumnOpen + `${item[key]}` + literatureColumnClose
+			dataToInsert +=
+				literatureColumnOpen + `${item[key]}` + literatureColumnClose
 		})
+		dataToInsert += literatureTrClose
 	})
-
 	dataToInsert += literatureTrClose
 
-	return dataToInsert
+	const literatureDataElement = document.getElementById('literature-data')
+	literatureDataElement.insertAdjacentHTML('beforeend', dataToInsert)
 }
-const literatureDataElement = document.getElementById('literature-data')
-literatureDataElement.insertAdjacentHTML('beforeend', getDataToInsert())
